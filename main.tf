220943258eaa8ad207493372463ddac7a99b1fba
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2025-07-14 18:44:46 +0200

    Now it'll also deploy to the management account

--- main.tf
+++ main.tf
@@ -44,10 +44,10 @@ resource "aws_cloudformation_stack_set" "resource_explorer" {
 }
 
 # ----------------------------
-# Single Account Deployment
+# Management account stack deployment | Single account
 # ----------------------------
 resource "aws_cloudformation_stack_set_instance" "single_account" {
-  for_each       = var.single_account_mode ? local.region_map : {}
+  for_each       = local.region_map
   stack_set_name = aws_cloudformation_stack_set.resource_explorer.name
   region         = each.value
   account_id     = var.management_account_id
