c964e0698398ee63818cc40227069e2137b9790c
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2025-07-22 13:33:04 +0200

    reverted back to just organisation stacksets

--- main.tf
+++ main.tf
@@ -3,23 +3,24 @@ data "aws_organizations_organization" "org" {}
 data "aws_regions" "available" {}
 
 locals {
-  # Determine enabled regions from input or from available commercial regions
   enabled_regions = var.regions != [] ? toset(var.regions) : toset([
     for r in data.aws_regions.available.names : r
     if r != "us-gov-east-1" && r != "us-gov-west-1" && r != "cn-north-1" && r != "cn-northwest-1"
   ])
 
-  # Default to root OU if no specific OU is passed
   root_ou_id   = data.aws_organizations_organization.org.roots[0].id
   target_ou_id = length(var.organization_ou_id) > 0 ? var.organization_ou_id : local.root_ou_id
 
-  # for_each requires a map, so we key the region names to themselves
   region_map = { for r in local.enabled_regions : r => r }
 }
 
-resource "aws_cloudformation_stack_set" "resource_explorer" {
-  name             = var.stackset_name
-  description      = "Deploys Resource Explorer index and view using CloudFormation"
+# ----------------------------
+# StackSet for OU-wide (excluding management account)
+# ----------------------------
+resource "aws_cloudformation_stack_set" "org_wide" {
+  count            = var.single_account_mode ? 0 : 1
+  name             = "${var.stackset_name}-org-wide"
+  description      = "Deploy Resource Explorer org-wide (excludes management account)"
   permission_model = "SERVICE_MANAGED"
 
   auto_deployment {
@@ -35,32 +36,20 @@ resource "aws_cloudformation_stack_set" "resource_explorer" {
     OrganizationArn       = data.aws_organizations_organization.org.arn
   }
 
-
   operation_preferences {
     region_concurrency_type = "PARALLEL"
     failure_tolerance_count = 1
   }
-
 }
 
-# ----------------------------
-# Management account stack deployment | Single account
-# ----------------------------
-# resource "aws_cloudformation_stack_set_instance" "single_account" {
-#   for_each       = local.region_map
-#   stack_set_name = aws_cloudformation_stack_set.resource_explorer.name
-#   region         = each.value
-#   account_id     = var.management_account_id
-# }
-
-# ----------------------------
-# Organization Deployment
-# ----------------------------
-resource "aws_cloudformation_stack_set_instance" "organization" {
+resource "aws_cloudformation_stack_set_instance" "org_instances" {
+  count          = var.single_account_mode ? 0 : 1
   for_each       = var.single_account_mode ? {} : local.region_map
-  stack_set_name = aws_cloudformation_stack_set.resource_explorer.name
+  stack_set_name = aws_cloudformation_stack_set.org_wide[0].name
   region         = each.value
+
   deployment_targets {
     organizational_unit_ids = [local.target_ou_id]
   }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "stackset_name" {
-  description = "Name of the deployed CloudFormation StackSet"
-  value       = aws_cloudformation_stack_set.resource_explorer.name
+output "org_stackset_name" {
+  description = "Name of the CloudFormation StackSet deployed to the organization (OU)"
+  value       = try(aws_cloudformation_stack_set.org_wide[0].name, null)
 }
+
+# output "management_stackset_name" {
+#   description = "Name of the CloudFormation StackSet deployed to the management account"
+#   value       = try(aws_cloudformation_stack_set.mgmt_account[0].name, null)
+# }
